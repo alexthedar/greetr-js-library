8570f5b838d84b9de4e7be18f8dd7727ed0500cf
alexthedar <alex.castaneda@gmail.com>
2015-12-08 12:15:32 -0800

    add supported languages and functions to prototype

--- Greetr.js
+++ Greetr.js
@@ -3,10 +3,47 @@
   var Greetr = function(firstName, lastName, language) {
 
     return new Greetr.init(firstName, lastName, language);
-    
+
   }
 
-  Greetr.prototype = {};
+  var supportLangs = ['es', 'en'];
+
+  var greetings = {
+      en: 'Hello',
+      es: 'Hola'
+  };
+
+  var formalGreetings = {
+    en: 'Greetings',
+    es: 'Saludos'
+  };
+
+  var logMessages = {
+    en: 'Logged in',
+    es: 'Inicio sesion'
+  };
+
+  Greetr.prototype = {
+
+    fullName: function() {
+      return this.firstName + ' ' + this.lastName;
+    },
+
+    validate: function() {
+      if (supportedLangs.indexOf(this.language) === -1) {
+        throw "Invalid Language";
+      }
+    },
+
+    greeting: function(){
+      return greetings[this.language] + ' ' + this.firstName + '!';
+    },
+
+    formalGreeting: function() {
+      return formalGreetings[this.language] + ', ' + this.fullName();
+    },
+
+  };
 
   Greetr.init = function(firstName, lastName, language){
 
